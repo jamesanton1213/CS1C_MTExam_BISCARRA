333ef55dbc116e690c3f67427a0697619b76d1df
jamesanton1213 <jamesanthonyragcabiscarra@gmail.com>
2025-03-21 21:26:35 +0800

    classSubjects.js

---
+++ classSubjects.js
@@ -0,0 +1,8 @@
+let studentName = prompt("Insert student name:");
+let subjectTitle = prompt("Insert subject title:");
+let classSchedule = prompt("Insert class schedule (Time, Days):");
+let classroom = prompt("Insert classroom:");
+let classInstuctor = prompt("Insert class instructor:");
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}.
+The instructor for the subject is ${classInstuctor}`);
