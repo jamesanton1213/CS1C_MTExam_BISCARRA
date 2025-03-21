6a180ed10c9b8ed10b6ba5eb2ddec3f9fe275aa6
jamesanton1213 <jamesanthonyragcabiscarra@gmail.com>
2025-03-21 21:21:44 +0800

    userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let FullName = prompt("Insert your Name");
+let Age = prompt("Insert your Age");
+let FavNum = prompt("Insert your favorite number");
+let FavColor = prompt("Insert your favorite color");
+
+console.log("Full name:", FullName);
+console.log("Age:", Age);
+console.log("Favorite Number:", FavNum);
+console.log("Favorite Color:", FavColor);
