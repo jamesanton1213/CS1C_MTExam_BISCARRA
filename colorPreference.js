113436c5b45eaff845d7c855460fa0127252512e
jamesanton1213 <jamesanthonyragcabiscarra@gmail.com>
2025-03-21 21:25:33 +0800

    colorPreference.js

---
+++ colorPreference.js
@@ -0,0 +1,7 @@
+let colors= [];
+
+for(let i = 0; i < 3; i++){
+    let FavColors = prompt("Insert your favorite color");
+    colors.push(FavColors);
+    console.log("Updated list of colors:", colors);
+}
